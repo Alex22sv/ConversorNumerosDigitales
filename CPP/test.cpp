0c206d31dee1d0585b45c6158728029ce4836aae
Alex22sv <ae22mp@gmail.com>
2023-04-11 15:24:22 -0600

    Primer día practicando con C++

---
+++ CPP/test.cpp
@@ -0,0 +1,32 @@
+// Se les llama directiva, el iostream siempre se incluye
+
+// El iostream (i de input, o de output) (entrada y salida de datos)
+#include <iostream>
+// Por defecto todas las estructuras. std es la abreviación de "estándar",
+//namespace es una palabra reservada que indica que usaremos bibliotecas. Lo veremos más adelante
+using namespace std;
+// Función principal del código. Si no es llamado dentro de main, no se ejecutará.
+int main(){
+    /*
+    Tipos de datos:
+        int (entero)
+        char (1 caracter)
+        string (múltiples caracteres)
+        double (decimal que incluye 64 bits)
+        float (decimal que incluye 32 bits)
+    */
+    // Definir una variable de tipo entero
+    int numero;
+    /*  
+    SALIDA DE DATOS
+        El cout no está dentro de la librería iostream, sino dentro del namespace std;
+        Si quiero utilizar el cout (o cin) sin utililizar el namespace std hacemos lo siguiente:
+        std::cout<<"Hello World!";
+    */
+    cout<<"Hello World";
+    // Leer número
+    cin>>numero;
+    // Si regresa 0, suele conocerse como que se ejecutó con éxito.
+    cout<<"El número ingresado fue " + to_string(numero);
+    return 0;
+}
