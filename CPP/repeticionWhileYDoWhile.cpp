dea275d25258094967125dda4ea24e4a7d1c05f1
Alex22sv <ae22mp@gmail.com>
2023-04-27 14:22:29 -0600

    Agregar correcciones al programa de While

--- CPP/repeticionWhileYDoWhile.cpp
+++ CPP/repeticionWhileYDoWhile.cpp
@@ -4,11 +4,18 @@
 using namespace std;
 
 int main(){
-    // Suma el rango de valores del 1 al 5 al final multiplicar 
-    i = 1;
-    totalRango = 0;
+    // Función para mostrar caracteres especiales y con acentos en terminal
+    SetConsoleOutputCP(CP_UTF8);
+
+    // Suma el rango de valores del 1 al 5 al final multiplicar por 3
+    int i = 1;
+    int totalRango = 0;
     while(i<=5){
+        totalRango += i;
+        i++;
     }
+    totalRango *= 3;
+    cout<<"La suma de los primeros 5 números y multiplicado por 3 es: "<<totalRango;
 
     return 0;
 }
