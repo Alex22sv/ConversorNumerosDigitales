72de55249fcafbd146f5bf2fdbd5dd5744c47a41
Alexander Morales <70553543+Alex22sv@users.noreply.github.com>
2023-04-27 08:43:55 -0600

    Ejercicio de while no terminado
    Lo terminaré por la tarde

---
+++ CPP/repeticionWhileYDoWhile.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main(){
+    // Suma el rango de valores del 1 al 5 al final multiplicar 
+    i = 1;
+    totalRango = 0;
+    while(i<=5){
+    }
+
+    return 0;
+}
