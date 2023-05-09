faa24b30d2548139f3747e43f184a35d75036256
Alex22sv <ae22mp@gmail.com>
2023-05-09 08:36:59 -0600

    Agregar práctica 9 de C++ con funciones

---
+++ CPP/practicaNueve.cpp
@@ -0,0 +1,31 @@
+// El valor del descuento será 35% del salario
+
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+float CalculoSalario(float salario);
+
+/* ¿Es posible tener 2 funciones con el mismo nombre?
+R// Sí, pero lo que va a cambiar son los parámetros.
+No puedes tener dos funciones con el mismo nombre sin parámetros
+Sí puedes tener dos funciones con el mismo nombre, una con parámetros y la otra no
+Se le llama "sobrecarga de parámetros" cuando tienes más de 1 función con el mismo nombre y que varía en cantidad de parámetros.
+ */
+
+int main(){
+    SetConsoleOutputCP(CP_UTF8);
+    float valorIngresado = 0;
+
+    cout<<"Ingrese su salario: $";
+    cin>>valorIngresado;
+    cout<<"El valor que le quitarán de la AFP es $"<<CalculoSalario(valorIngresado);
+    return 0;
+}
+
+
+float CalculoSalario(float salario){
+    float total = salario * 0.35;
+    return total;
+}
