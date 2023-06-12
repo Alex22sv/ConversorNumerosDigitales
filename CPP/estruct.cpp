a1c16c442ee87f26c1fee7688e8d73a283c5dc1b
Alex22sv <ae22mp@gmail.com>
2023-06-12 16:58:01 -0600

    Actualizar ejemplo de estructuras

--- CPP/estruct.cpp
+++ CPP/estruct.cpp
@@ -5,12 +5,18 @@ using namespace std;
 
 // Dentro de una estructura no se llaman variables, sino campos (son miembros de la estructura)
 struct Alumno{
-    // string nombre;
-    char name[25];
+    string name;
+    //char name[25];
     int year;
     float grades[3];
-}alumno1, alumno2; // Primer forma de crear variables de tipo struct
+} alumno1, alumno2; // Primer forma de crear variables de tipo struct
 
+struct Libros{
+    string autor;
+    string titulo;
+    int publicacion;
+    string editores[2];
+} libro[3];
 
 int main(){
     // Determinar el tamaño en bytes de la estructura
@@ -36,6 +42,23 @@ int main(){
     };
     
     // Imprimir
-    cout<<alumno1.name<<" - "<<alumno1.year;
+    cout<<alumno1.name<<" - "<<alumno1.year<<"\n\n";
+
+    // Arreglos y estructuras
+    for (int i = 0; i < 3; i++){
+        cout<<"Ingrese el nombre del autor: ";
+        cin>>libro[i].autor;
+        cout<<"Ingrese el título: ";
+        cin>>libro[i].titulo;
+        cout<<"Ingrese la fecha de publicación: ";
+        cin>>libro[i].publicacion;
+        for (int j = 0; j < 2; j++){
+            cout<<"Ingrese la editora #"<<j+1;
+            cin>>libro[i].editores[j];
+        }
+        
+        
+    }
+    
     return 0;
 }
